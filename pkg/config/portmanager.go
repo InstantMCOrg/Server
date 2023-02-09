6d051fd50f228dd616d1df7c9c7acd4d2402fa82
Binozo <70137898+Binozo@users.noreply.github.com>
2023-02-09 21:34:22 +0100

    added portmanager and other utilities

---
+++ pkg/config/portmanager.go
@@ -0,0 +1,6 @@
+package config
+
+const (
+	PortRangeBegin = 25000
+	PortRangeEnd   = 25900
+)
---
+++ pkg/manager/portmanager.go
@@ -0,0 +1,40 @@
+package manager
+
+import (
+	"github.com/instantminecraft/server/pkg/config"
+	"github.com/instantminecraft/server/pkg/utils"
+)
+
+var usedPorts []int
+
+func GeneratePort() int {
+	port := utils.CreateRandomIntRange(config.PortRangeBegin, config.PortRangeEnd)
+	if IsPortBeingUsed(port) {
+		return GeneratePort()
+	}
+	return port
+}
+
+func IsPortBeingUsed(port int) bool {
+	for _, usedPort := range usedPorts {
+		if usedPort == port {
+			return true
+		}
+	}
+	return false
+}
+
+func AddPortToUsageList(port int) {
+	usedPorts = append(usedPorts, port)
+}
+
+func RemovePortFromUsageList(port int) {
+	var tempUsedPorts []int
+
+	for _, usedPort := range usedPorts {
+		if usedPort != port {
+			tempUsedPorts = append(tempUsedPorts, usedPort)
+		}
+	}
+	usedPorts = tempUsedPorts
+}
