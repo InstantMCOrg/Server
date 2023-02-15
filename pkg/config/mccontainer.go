0023723b2541ccffc43a65af22573f73c5d63790
Binozo <70137898+Binozo@users.noreply.github.com>
2023-02-15 07:00:35 +0100

    added more util stuff

--- pkg/config/mccontainer.go
+++ pkg/config/mccontainer.go
@@ -3,15 +3,19 @@ package config
 import "strconv"
 
 const (
-	BaseImageName     = "ghcr.io/instantminecraft/client"
-	ContainerBaseName = "MC-Server-"
-	McServerProxyPort = 25585
+	BaseImageName = "ghcr.io/instantminecraft/client"
+
+	McVersionSuffix = ":mc-"
+
+	BaseVanillaMcImageName = BaseImageName + McVersionSuffix
+	ContainerBaseName      = "MC-Server-"
+	McServerProxyPort      = 25585
 )
 
 var (
 	AvailableVersions         = [...]string{"1.19.3", "1.19.2", "1.19", "1.18.2", "1.18", "1.17", "1.16.5", "1.16", "1.15.2", "1.15", "1.14.4", "1.14", "1.13.2", "1.13", "1.12.2", "1.12", "1.11.2", "1.11", "1.10.2", "1.9.4", "1.9", "1.8.9", "1.8", "1.7.10", "1.7"}
 	WaitingReadyContainerName = ContainerBaseName + "ready"
-	LatestImageName           = "ghcr.io/instantminecraft/client:mc-" + AvailableVersions[0]
+	LatestImageName           = "ghcr.io/instantminecraft/client" + McVersionSuffix + AvailableVersions[0]
 )
 
 func WaitingReadyContainerNr(containerNumber int) string {
--- pkg/utils/random.go
+++ pkg/utils/random.go
@@ -1,7 +1,18 @@
 package utils
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func CreateRandomIntRange(min int, max int) int {
 	return rand.Intn(max-min) + min
+}
+
+func RandomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, length+2)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)[2 : length+2]
 }
