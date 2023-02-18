96a1bc241576f49ca3796f97dcc59cb47d27dbd8
Binozo <70137898+Binozo@users.noreply.github.com>
2023-02-18 14:00:48 +0100

    made some changes to config stuff

--- pkg/config/mccontainer.go
+++ pkg/config/mccontainer.go
@@ -13,11 +13,16 @@ const (
 )
 
 var (
-	AvailableVersions         = [...]string{"1.19.3", "1.19.2", "1.19", "1.18.2", "1.18", "1.17", "1.16.5", "1.16", "1.15.2", "1.15", "1.14.4", "1.14", "1.13.2", "1.13", "1.12.2", "1.12", "1.11.2", "1.11", "1.10.2", "1.9.4", "1.9", "1.8.9", "1.8", "1.7.10", "1.7"}
+	AvailableVersions         = []string{"1.19.3", "1.19.2", "1.19", "1.18.2", "1.18", "1.17", "1.16.5", "1.16", "1.15.2", "1.15", "1.14.4", "1.14", "1.13.2", "1.13", "1.12.2", "1.12", "1.11.2", "1.11", "1.10.2", "1.9.4", "1.9", "1.8.9", "1.8", "1.7.10", "1.7"}
 	WaitingReadyContainerName = ContainerBaseName + "ready"
-	LatestImageName           = "ghcr.io/instantminecraft/client" + McVersionSuffix + AvailableVersions[0]
+	LatestMcVersion           = AvailableVersions[0]
+	LatestImageName           = BaseImageName + McVersionSuffix + LatestMcVersion
 )
 
 func WaitingReadyContainerNr(containerNumber int) string {
 	return WaitingReadyContainerName + "-" + strconv.Itoa(containerNumber)
+}
+
+func ImageWithMcVersion(mcVersion string) string {
+	return BaseImageName + McVersionSuffix + mcVersion
 }
