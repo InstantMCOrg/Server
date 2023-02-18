ec0491c692cc429d2999a3fb4ac8eb10dcc1f6ae
Binozo <70137898+Binozo@users.noreply.github.com>
2023-02-18 14:01:04 +0100

    added ToClientJson func for McServerContainer struct

--- pkg/models/mcserver.go
+++ pkg/models/mcserver.go
@@ -9,6 +9,22 @@ type McServerContainer struct {
 	Running     bool   `json:"running"`
 }
 
+func (mcServer *McServerContainer) ToClientJson() interface{} {
+	return struct {
+		ID        string `json:"id"`
+		Name      string `json:"name"`
+		McVersion string `json:"mc_version"`
+		Port      int    `json:"port"`
+		Running   bool   `json:"running"`
+	}{
+		ID:        mcServer.ID,
+		Name:      mcServer.Name,
+		McVersion: mcServer.McVersion,
+		Port:      mcServer.Port,
+		Running:   mcServer.Running,
+	}
+}
+
 type ServerStatus struct {
 	Server struct {
 		Running bool `json:"running"`
