a154d87dc402d63e24e729b1d2563458575c6847
Binozo <70137898+Binozo@users.noreply.github.com>
2023-02-19 12:59:15 +0100

    added forced password change (default user account admin:admin)

--- pkg/api/router/userroutes.go
+++ pkg/api/router/userroutes.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"encoding/json"
+	"github.com/instantminecraft/server/pkg/config"
 	"github.com/instantminecraft/server/pkg/db"
 	"github.com/instantminecraft/server/pkg/utils"
 	"net/http"
@@ -17,9 +18,9 @@ func loginRoute(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// check if user exists
-	password = utils.SHA256([]byte(password))
+	hashedPassword := utils.SHA256([]byte(password))
 
-	user, err := db.Login(username, password)
+	user, err := db.Login(username, hashedPassword)
 	if err != nil {
 		// User doesn't exist
 		sendError("Invalid credentials", w, http.StatusUnauthorized)
@@ -36,7 +37,8 @@ func loginRoute(w http.ResponseWriter, r *http.Request) {
 
 	// Session successfully created
 	data, _ := json.Marshal(map[string]interface{}{
-		"token": token,
+		"token":                    token,
+		"password_change_required": password == config.PasswordRequiresChange,
 	})
 	w.WriteHeader(http.StatusOK)
 	w.Write(data)
--- pkg/config/other.go
+++ pkg/config/other.go
@@ -1,3 +1,5 @@
 package config
 
 const DataDir = "data"
+
+const PasswordRequiresChange = "admin" // a summit of all passwords which are not allowed and need to be changed
